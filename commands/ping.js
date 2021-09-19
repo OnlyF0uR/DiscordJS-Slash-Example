b1ed09c1f0eb041a653f6dcf494881e30d199c83
Jerry Blaauw <jerskisnow@gmail.com>
2021-09-19 20:32:17 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -19,7 +19,7 @@ const execute = async function(client, interaction) {
  *
  * This allows you to keep the button actions for each command in the command class.
  * So if someones pressed a button you created then it will lead back to the function
- * ~ defined in the button object. The button-event-data will get copied to the botCache on bot startup.
+ * ~ defined in the button object. The button-event data will get copied to the botCache on bot startup.
  *
  * Be sure to include 'buttons: buttonActions', within the module.exports at the bottom of the file.
  */
